2ee1cc7790432fb7e7062c9c0e656f83dd7cd558
Hosseinjozarian <129601724+Hosseinjozarian@users.noreply.github.com>
2023-12-06 01:19:23 +1100

    Update fghfg.py

--- fghfg.py
+++ fghfg.py
@@ -1,3 +1,14 @@
-import pandas
+import numpy as np
+import yfinance as yf
+import pandas as pd
 
+# Importation of data
+list_tickers = ["AAPL"]
+data = yf.download(list_tickers)
+
+# For tick calculation(The smallest unit of change)
+for i in range(0, len(data)):
+    data['tick'] = data['Close'] - data['Open']
+
+print(data)
 
